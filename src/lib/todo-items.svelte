8c066493f6e9b582afb487f31388baa0c7ab9597
Adithya D.M <adithya.daine@icloud.com>
2022-02-26 17:27:13 +0530

    Created a work-in progress HTML

---
+++ src/lib/todo-items.svelte
@@ -0,0 +1,16 @@
+<div>
+	<form action="" method="">
+		<input type="hidden" name="done" value="" />
+		<button aria-label="Mark done/not done">Done/NotDone</button>
+	</form>
+
+	<form action="" method="">
+		<input type="text" name="" />
+		<button aria-label="Save button">Save</button>
+	</form>
+
+	<form action="" method="">
+		<input type="text" name="" />
+		<button aria-label="Delete button">Delete</button>
+	</form>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import TodoItems from '$lib/todo-items.svelte';
+
+	const title = 'Todo';
+</script>
+
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<!--Creating a form-->
+<form action="" method="">
+	<input
+		type="text"
+		name="text"
+		value=""
+		aria-label="Add a todo"
+		placeholder="+ tap to add a todo"
+	/>
+</form>
+
+<!--Svelte components-->
+<TodoItems />
